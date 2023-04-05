2eaeea43f126c1004b638fb4e08a5c5180f3b7b1
Matvei Khmyzov <matw.hmizov@yandex.ru>
2023-04-05 14:57:52 +0300

    Added secret app

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -109,13 +109,13 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 #     }
 # }
 
-# DATABASE_URL = os.getenv('DATABASE_URL')
+DATABASE_URL = os.getenv('DATABASE_URL')
 DATABASES = {
-    # 'default': dj_database_url.parse(DATABASE_URL)
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600,
-    )
+    'default': dj_database_url.parse(DATABASE_URL)
+    # 'default': dj_database_url.config(
+    #     default='sqlite:///db.sqlite3',
+    #     conn_max_age=600,
+    # )
 }
 
 
