325495e30380f18b9022b18e71c725ad677f03f0
Matvei Khmyzov <matw.hmizov@yandex.ru>
2023-04-19 10:43:43 +0300

    Fixed created error

--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -7,7 +7,7 @@ from .views import (TaskIndex,
 
 urlpatterns = [
     path('', TaskIndex.as_view(), name='task_list'),
-    # path('create', CreateTask.as_view(), name='create_task'),
+    path('create', CreateTask.as_view(), name='create_task'),
     path('<int:pk>', TaskCard.as_view(), name='task_card'),
     path('<int:pk>/update', UpdateTask.as_view(), name='update_task'),
     path('<int:pk>/delete', DeleteTask.as_view(), name='delete_task')
